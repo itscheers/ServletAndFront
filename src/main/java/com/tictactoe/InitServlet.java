aee8bf92b9a6a7c210a26ce15ee3b3da6fa33102
Andrii <savchenko.andrii05@gmail.com>
2025-06-27 21:26:29 +0300

    gitignore changes

--- src/main/java/com/tictactoe/InitServlet.java
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -14,22 +14,16 @@ import java.util.Map;
 public class InitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Створення нової сесії
         HttpSession currentSession = req.getSession(true);
 
-        // Створення ігрового поля поля
         Field field = new Field();
         Map<Integer, Sign> fieldData = field.getField();
 
-        // Отримання списку значень поля
         List<Sign> data = field.getFieldData();
 
-        // Додавання до сесії параметрів поля (буде потрібно для зберігання стану між запитами)
         currentSession.setAttribute("field", field);
-        // і значень поля, відсортованих за індексом (потрібно для промалювання хрестиків і нуликів)
         currentSession.setAttribute("data", data);
 
-        // Перенаправлення запиту на сторінку index.jsp через сервер
         getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
